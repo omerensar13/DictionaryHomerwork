6bceecfdd0ea61a81ceb66037c7410362b1293ac
omerensar13 <omerensar@hotmail.com.tr>
2021-01-26 16:56:46 +0100

    Add project files.

---
+++ DictionaryHomerwork/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DictionaryHomerwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> myDictionary = new MyDictionary<string> ();
+            myDictionary.Add("Adem");
+            myDictionary.Add("Halil");
+            myDictionary.Add("Ömer");
+            Console.WriteLine(myDictionary.Lenght);
+            foreach (var isimler in myDictionary.Items)
+            {
+                Console.WriteLine(isimler);
+            }
+            
+        }
+    }
+}
